452272b161b914214d19018386d63348c03a4a71
rohdma02 <rohdma02@luther.edu>
2024-08-07 12:23:58 -0500

    feat: ensure barbershop servoces are shown on barbershop page

--- fsw-barber/app/barbershops/[id]/page.tsx
+++ fsw-barber/app/barbershops/[id]/page.tsx
@@ -1,18 +1,35 @@
+import ServiceItem from "@/app/_components/service-item"
 import { Button } from "@/app/_components/ui/button"
 import { db } from "@/app/_lib/prisma"
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import { notFound } from "next/navigation"
 
-const BarbershopPage = async ({ params }) => {
+interface BarbershopPageProps {
+    params: {
+        id: string
+    }
+}
+
+
+const BarbershopPage = async ({ params }: BarbershopPageProps) => {
     const barbershop = await db.barbershop.findUnique({
         where: {
             id: params.id,
+        },
+        include: {
+            services: true
         }
     })
 
+
+
+    if (!barbershop) {
+        return notFound()
+    }
     return (
-        
+
         <div>
             {/* IMAGEM */}
             <div className="relative w-full h-[250px]">
@@ -37,13 +54,23 @@ const BarbershopPage = async ({ params }) => {
                     <p className="text-sm">Avaliacoes em progresso</p>
                 </div>
             </div>
-        
 
-        {/* DESCRICAO */}
-        <div className="p-5 border-b border-solid space-y-3">
-            <h2 className="font-bold uppercase text-gray-400 text-sm">Sobre nos</h2>
-            <p className="text-justify text-sm">{barbershop?.description}</p>
-        </div>
+
+            {/* DESCRICAO */}
+            <div className="p-5 border-b border-solid space-y-3">
+                <h2 className="font-bold uppercase text-gray-400 text-sm">Sobre nos</h2>
+                <p className="text-justify text-sm">{barbershop?.description}</p>
+            </div>
+
+
+            <div className="p-5 space-y-3">
+                <h2 className="text-xs font-bold uppercase text-gray-400 ">Servicos</h2>
+                <div className="space-y-3">
+                    {barbershop.services.map((service) => (
+                        <ServiceItem key={service.id} service={service} />
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
